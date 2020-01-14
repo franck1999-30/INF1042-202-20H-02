741e9bdc4b20371b9a0e01fc9d6ed35d21a73efd
Raissa111 <ernanjia@gmail.com>
2020-01-14 15:48:59 -0500

    En Cours

---
+++ 1.Introduction/300117782.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 15:15:58 2020
+
+@author: User
+"""
+
+print ( 'Bonne Année' )
+
+for v in range ( 10 ):
+    print(' Bonne Année')
--- 1.Introduction/README.md
+++ 1.Introduction/README.md
@@ -1,2 +1,9 @@
-# Introduction
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 15:13:16 2020
 
+@author: User
+"""
+
+print ( "Bonne Année ")
+p
