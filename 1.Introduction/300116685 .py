eabce2b7ffdaad9da7bd3a1c53cff8d94d9081af
amirali175 <amirdek511@gmail.com>
2020-01-14 15:42:09 -0500

    :pushpin:

---
+++ 1.Introduction/300116685 .py
@@ -0,0 +1,9 @@
+"""je veux creer une petite programmer   """
+
+# affiche bonnee Annee a 100 fois
+
+print(10*'bonnee Annee my teameclass\n')
+for i in range(0,10):
+    print('Bonnee Annee my teamclass')
+
+
