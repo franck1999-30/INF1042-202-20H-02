63a994cbeb47de367590523d1bd5392ff087d14f
Auriane25 <kdmarieauriane@gmail.com>
2020-01-14 15:45:36 -0500

    :smile:

--- 1.Introduction/sanstitre0.py
+++ 1.Introduction/sanstitre0.py
@@ -4,4 +4,4 @@ Created on Tue Jan 14 15:15:20 2020
 
 @author: kdmar
 """
-print ('Bonne annee')
+print   (10 * 'Bonne annee\n') #O(1)
---
+++ 1.Introduction/sanstitre1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 15:22:26 2020
+
+@author: kdmar
+"""
+
+len('aurianemar')*('Bonne annee')
