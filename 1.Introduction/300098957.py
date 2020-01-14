df3020643cd36b1ace88de436d5faf6ec3bd5a23
Setrar <setrar@valiha.com>
2020-01-14 15:35:36 -0500

    :pushpin:

--- 1.Introduction/300098957.py
+++ 1.Introduction/300098957.py
@@ -5,4 +5,20 @@ Created on Tue Jan 14 15:13:07 2020
 @author: 300098957
 """
 
-print('Bonne annee')
+print(10 * 'Bonne annee \n') # O(1)
+
+for i in range(0,10):   # O(log n)
+    print('Bonne annee ')
+
+print() # Passe a la ligne
+
+print('Bonne annee ') # O(n)
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
+print('Bonne annee ')
