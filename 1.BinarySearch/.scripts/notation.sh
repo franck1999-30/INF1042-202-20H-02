44dd67fb727419025c43639a2d13e346a0454a5f
setrar <setrar@valiha.com>
2020-01-20 14:43:42 -0500

    Update notation.sh

--- 1.BinarySearch/.scripts/notation.sh
+++ 1.BinarySearch/.scripts/notation.sh
@@ -22,5 +22,15 @@ do
 
    echo "\`\`\`"
    echo "###  =[END]============== Exécution du script ${id} =============== "
+   
+   
+   echo "import sys; sys.path.append('.') # Rajouter le repertoire courant" > .scripts/${id}.spec.py
+  
+   echo "from b300098957 import binary_search" >> .scripts/${id}.spec.py
+   echo "test = binary_search( [5,7,11,14,19,20,25], 11) == 2 " >> .scripts/${id}.spec.py
+   echo "if  test:" >> .scripts/${id}.spec.py
+   echo "     print('Good')" >> .scripts/${id}.spec.py
+   echo "else:" >> .scripts/${id}.spec.py
+   echo "     print('Bad')" >> .scripts/${id}.spec.py
 
 done
