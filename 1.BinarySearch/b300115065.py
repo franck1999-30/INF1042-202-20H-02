b02dbfeda9bf70ff3b5dcc11e93dedba4e6d8104
fadde68 <54910778+fadde68@users.noreply.github.com>
2020-01-21 14:08:32 -0500

    Update b300115065.py

--- 1.BinarySearch/b300115065.py
+++ 1.BinarySearch/b300115065.py
@@ -1,24 +1,29 @@
+
+"""
+
 Created on Wed Jan 15 15:28:59 2020
 
 @author: akram fadde
 """
 
 
-
-
-def binary_search(list, item):
-   # low = 0
-   # high = len(list)-1
+def binary_search (list, item):
+    low = 0
+    high = len(list)-1
     
-   # while low <= high :
-        #mid = (low + high) / 2
-        #guess = list(mid)
-        #if guess :
-            
-    return item
+    while low <= high: 
+        mid = (low + high) 
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item :
+           high = mid - 1
+        else : 
+           low = mid + 1 
+    return None
+
 
 my_list = [5,7,11,14,19,20,25]
 
 trouve = 11
-
 print( binary_search(my_list, trouve))
